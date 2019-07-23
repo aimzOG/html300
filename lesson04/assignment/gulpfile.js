b3efb6f4481491f99cdc948fafac6e54459fa18e
aimzOG <aimz93@gmail.com>
2019-07-23 13:41:27 -0700

    replaces package-json into assignments

--- lesson04/assignment/gulpfile.js
+++ lesson04/assignment/gulpfile.js
@@ -1,37 +1,36 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    autoprefix = require('gulp-autoprefixer'),
-    watch = require('gulp-watch'),
-    plumber = require('gulp-plumber'),
-    gutil = require('gulp-util'),
-    browserSync = require('browser-sync').create();
+  sass = require('gulp-sass'),
+  autoprefix = require('gulp-autoprefixer'),
+  watch = require('gulp-watch'),
+  plumber = require('gulp-plumber'),
+  browserSync = require('browser-sync').create();
 
 gulp.task('sass', function () {
-    gulp.src('css/*.scss')
-        .pipe(plumber())
-        .pipe(sass())
-        .pipe(
-            autoprefix({
-                browsers: ['> .5%'],
-            })
-        )
-        .pipe(minifycss({ compatibility: 'ie8' }))
-        .pipe(gulp.dest('css/'))
-        .pipe(browserSync.stream());
+  gulp.src('css/*.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(
+      autoprefix({
+        browsers: ['> .5%'],
+      })
+    )
+    .pipe(minifycss({ compatibility: 'ie8' }))
+    .pipe(gulp.dest('css/'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function () {
-    gulp.watch('css/*.scss', ['sass']);
-    gulp.watch('*.html').on('change', browserSync.reload);
+  gulp.watch('css/*.scss', ['sass']);
+  gulp.watch('*.html').on('change', browserSync.reload);
 });
 
 gulp.task('browser-sync', function () {
-    browserSync.init({
-        notify: false,
-        server: {
-            baseDir: './',
-        },
-    });
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: './',
+    },
+  });
 });
 
 gulp.task('default', ['watch', 'browser-sync']);
